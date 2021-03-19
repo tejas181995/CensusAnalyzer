9a54ef8569f39f465c7542069060902416acda26
tejas181995 <tejasdev95@gmail.com>
2021-03-19 18:19:45 +0530

    new file created

---
+++ src/main/java/com/bridgelabz/censusanalyser/CensusAnalyser.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.censusanalyser;
+
+public class CensusAnalyser {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Census analyser Problem");
+    }
+}
